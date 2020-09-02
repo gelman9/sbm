602095ccf795d2e94c32ed3ae02537ca938f259e
gelman <gelman9@gmail.com>
2020-09-02 18:56:25 +0300

    dev 5 commit

--- SBM/Startup.cs
+++ SBM/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace SBM
+namespace SBM//1
 {
     public class Startup
     {
